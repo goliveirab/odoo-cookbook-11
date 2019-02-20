0615d5e2d08acd64534a71e396caeacada6d82e9
Germana Oliveira <germana@vauxoo.com>
2019-02-19 20:47:18 -0400

    [ADD] library_book: Creating and installing new module  (#1)
    * [ADD] library_book: Creating and installing new module
    
    * Create and install the new module
    * Completing the __manifest__
    * Adding models
    * Adding menu items and views
    
    * [REF] change module name

---
+++ my_library_book/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Book Library",
+    'summary': "This module allows to manage a simple book library",
+    'description': """This module was done to practice with odoo cookbook. The
+    Odoo Cookbook used refer to v11 but ths code was made and tested in v12""",
+    'author': "Alexander Fayolle, Germana Oliveira",
+    'license': "AGPL-3",
+    'website': "https://github.com/goliveirab/odoo-cookbook-11",
+    'category': 'Uncategorized',
+    'version': '12.0.1.0.0',
+    'depends': ['base'],
+    'data': [
+        'views/library_book_view.xml',
+    ],
+    'demo': [],
+}
